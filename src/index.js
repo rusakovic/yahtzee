df4ba7b98c2df19d51df326c61d2809d0c7066f3
maxrusakovich <maxrusakovich@gmail.com>
2019-09-03 10:48:34 +0300

    change fontawesome minify css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 import registerServiceWorker from './registerServiceWorker';
 
